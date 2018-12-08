c31ea4bfb26a02b05e3cab96b61665a0cf3cadb7
Nicolas Cellier <nicolas.cellier.aka.nice@gmail.com>
2018-12-08 17:00:33 +0100

    Don't shift an unsigned by 32 positions.
    This is clearly defined as UB by the standard.
    This happens during smoke tests when compiled with `-fsanitize=shift`, so it's not just theoretical.
    The compiler might decide that it can omit the test, knowing about implementation details.
    But in portable source code, we cannot make this assumption.

--- e_sqrt.c
+++ e_sqrt.c
@@ -114,8 +114,10 @@ double __ieee754_sqrt(double x)
             }
             for(i=0;(ix0&0x00100000)==0;i++) ix0<<=1;
             m -= i-1;
-            ix0 |= (ix1>>(32-i));
-            ix1 <<= i;
+            if(i) { /* protect: shift>>32 is undefined */
+                ix0 |= (ix1>>(32-i));
+                ix1 <<= i;
+            }
         }
         m -= 1023;      /* unbias exponent */
         ix0 = (ix0&0x000fffff)|0x00100000;
